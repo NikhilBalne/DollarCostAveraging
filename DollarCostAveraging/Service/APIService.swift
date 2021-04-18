da39b5093a12b1564431e78c7d0ca8c4019db025
Nikhilkumar Balne <nbalne@aligntech.com>
2021-04-18 12:12:53 +0530

    4.Get API Keys.

---
+++ DollarCostAveraging/Service/APIService.swift
@@ -0,0 +1,16 @@
+//
+//  APIService.swift
+//  DollarCostAveraging
+//
+//  Created by Nikhilkumar Balne on 18/04/21.
+//
+
+import Foundation
+
+struct APIService {
+    
+    let APIKey = "Y6YJF9VQPF4MWYEF"
+    let APIKey2 = "JDFKPHLVMVHH6ZNW"
+    let APIKey3 = "BIVKCG7LC48QC62Y"
+    
+}
